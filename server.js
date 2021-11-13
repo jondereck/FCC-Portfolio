43d94457efe545d2b971e7f175afa121826ec6cc
jondereck <jonderecknifas@gmail.com>
2021-11-13 20:02:36 +0800

    Upgrade server.js

--- server.js
+++ server.js
@@ -8,8 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-const { request, response } = require('express');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -32,32 +31,32 @@ var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
 
-let responseObject = {}
-app.get('/api/:input', (request, response) => {
-  let input = request.params.input
-  if(input.includes('-')){
-    /*date string*/
-    responseObject['unix'] = new Date(input).getTime()
-    responseObject['utc'] = new Date(input).toUTCString()
-  }else{
-    /*timestamp*/
-    input = parseInt(input)
-    responseObject['unix'] = new Date(input).getTime()
-    responseObject['utc'] = new Date(input).toUTCString()
+
+// API main endpoint
+app.get("/api/", function (req, res) {
+  let dateApiObject = {
+    unix: new Date().getTime(),
+    utc: new Date().toUTCString(),
+  };
+  res.json(dateApiObject);
+});
+
+// API endpoint for date parameters
+app.get("/api/:date", function (req, res) {
+  let date_string = req.params.date;
+  if (new Date(date_string) == "Invalid Date" && isNaN(date_string)) {
+    res.json({ error: "invalid date" });
+  } else if (isNaN(date_string)) {
+    res.json({
+      unix: new Date(date_string).getTime(),
+      utc: new Date(date_string).toUTCString(),
+    });
+  } else {
+    date_string = parseInt(date_string);
+    res.json({
+      unix: new Date(date_string).getTime(),
+      utc: new Date(date_string).toUTCString(),
+    });
   }
+});
 
-  if(!responseObject['unix'] || !responseObject['utc']){
-    response.json({error: 'Invalid Date'})
-  }
-
-  response.json(responseObject)
-})
-
-
-
-app.get('/api/', (request, response) => {
-  responseObject['unix'] = new Date().getTime()
-  responseObject['utc'] = new Date().toUTCString()
-  
-  response.json(responseObject)
-})
