954798c8e084ebe3a22bceeffad74039ed7387c5
jondereck <jonderecknifas@gmail.com>
2021-11-14 19:45:20 +0800

    Upgrade server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -26,6 +26,32 @@ mongoose.connect(process.env.MONGO_URI, {
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => console.log("MongoDB connection established \n"));
+
+
+// Schema
+const usersSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  count: {
+    type: Number,
+  },
+  log: [
+    {
+      description: { type: String },
+      duration: { type: Number },
+      date: { type: Date },
+    },
+  ],
+});
+
+// Model
+const User = mongoose.model("User", usersSchema);
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 
 
@@ -47,6 +73,109 @@ app.get("/", function (req, res) {
 });
 
 
+// * Users API
+
+app.post("/api/users", (req, res) => {
+  if (req.body.username === "") {
+    res.json({ error: "Please enter username" });
+  } else {
+    let searchUser = User.findOne(
+      { username: req.body.username },
+      (err, searchResult) => {
+        if (searchResult) {
+          res.json({ error: "Username already taken" });
+        } else {
+          let newUser = new User({
+            username: req.body.username,
+          });
+          newUser.save();
+          res.json({
+            _id: newUser._id,
+            username: newUser.username,
+          });
+        }
+      }
+    );
+  }
+});
+
+// * POST Exercise
+
+app.post("/api/users/:_id/exercises", (req, res) => {
+  var formattedDate;
+  if (req.body.date === "") {
+    formattedDate = new Date().toDateString();
+  } else {
+    formattedDate = new Date(req.body.date).toDateString();
+  }
+
+  let exerciseEntry = {
+    date: formattedDate,
+    duration: parseInt(req.body.duration),
+    description: req.body.description,
+  };
+
+  User.findByIdAndUpdate(
+    { _id: req.body[":_id"] },
+    { $push: { log: exerciseEntry } },
+    { new: true },
+    (err, updatedUserObject) => {
+      if (err) {
+        console.error(err);
+        res.send(err);
+      } else {
+        updatedUserObject.count = updatedUserObject.log.length;
+        updatedUserObject.date = formattedDate;
+        console.log(updatedUserObject);
+        res.json({
+          _id: updatedUserObject._id,
+          username: updatedUserObject.username,
+          date: exerciseEntry.date,
+          duration: exerciseEntry.duration,
+          description: exerciseEntry.description,
+        });
+      }
+    }
+  );
+});
+
+/*
+ * GET Users Method
+ */
+
+app.get("/api/users", async (req, res) => {
+  await User.find({}, (err, resultDocs) => {
+    if (err) {
+      console.error(err);
+      res.send(err);
+    } else {
+      console.log(resultDocs);
+      res.json(resultDocs);
+    }
+  });
+});
+
+/*
+ * GET Users with ID and their logs
+ */
+app.get("/api/users/:_id/logs", async (req, res) => {
+  await User.findById(req.params["_id"], (err, searchResult) => {
+    if (err) {
+      console.error(err);
+      res.send(err);
+    } else {
+      res.json({
+        _id: searchResult._id,
+        username: searchResult.username,
+        count: searchResult.log.length,
+        log: searchResult.log,
+      });
+    }
+  });
+});
+
+
+
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
@@ -65,6 +194,17 @@ app.get("/parser", function (req, res) {
 app.get("/shorten", function (req, res) {
   res.sendFile(__dirname + '/views/shorten.html');
 });
+
+
+app.get("/tracker", function (req, res) {
+  res.sendFile(__dirname + '/views/tracker.html');
+});
+
+
+app.get("/metadata", function (req, res) {
+  res.sendFile(__dirname + '/views/metadata.html');
+});
+
 
 
 
@@ -191,6 +331,4 @@ app.get('/api/shorturl/:id', (req, res) => {
     })
   }
 })
-
-
 
